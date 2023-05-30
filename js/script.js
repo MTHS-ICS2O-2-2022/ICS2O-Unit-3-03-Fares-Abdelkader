24c502fc17c4e74fb4065db28978c74d17d38e68
Fares-Abdelkader <124618781+Fares-Abdelkader@users.noreply.github.com>
2023-05-30 17:19:47 +0000

    Index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fares.Abdelkader
+// Created on: March 2022
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates volume of sphere.
+ */
+function calculate() {
+  // input
+  const radius = parseInt(document.getElementById('radius-sphere').value)
+
+  // process 
+  const volume = 4 / 3 * Math.PI *  Math.pow(radius, 3)
+
+  // output
+  document.getElementById('volume').innerHTML = 'volume is: ' + volume + 'cm³'
+}
